8932438e43b70e63f902a7effb830843135728fb
Aigerim Raimbay <raimbay89@gmail.com>
2024-04-14 12:10:09 -0500

    adding values code in nasm

--- add_values/adding_values.asm
+++ add_values/adding_values.asm
@@ -1,7 +1,12 @@
     global _main
     section .text
+
 _main:
-    mov rax, 3      
-    add rdi, 5
-    mov rax, 0x02000001
+    mov rax, 0x02000004      ; system call for write
+    mov rdi, 3
+    mov rsi, 5
+    add rdi, rsi
+    syscall
+
+    mov rax, 0x02000001      ; system call to exit
     syscall
--- hello_world/hello_world.asm
+++ hello_world/hello_world.asm
@@ -6,9 +6,11 @@ _main:
     mov rsi, message
     mov rdx, 13
     syscall
+
     mov rax, 0x02000001
     xor rdi, rdi
     syscall
+     
     section .data
 message:
     db  "Hello, World", 10
